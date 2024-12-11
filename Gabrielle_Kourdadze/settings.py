c2a18cb84ed480aef941ee4f7c4e709e0df32f88
Kakha Kakhadze <k.kakhadze.work@gmail.com>
2024-12-11 23:27:02 +0400

    domain

--- Gabrielle_Kourdadze/settings.py
+++ Gabrielle_Kourdadze/settings.py
@@ -31,10 +31,13 @@ CSRF_COOKIE_SECURE = True
 
 CSRF_USE_SESSIONS = True
 
-ALLOWED_HOSTS = ["lobster-app-fy7pe.ondigitalocean.app", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["gabriellekourdadze.com", "www.gabriellekourdadze.com/", "http://www.gabriellekourdadze.com/", "lobster-app-fy7pe.ondigitalocean.app", "127.0.0.1", "localhost"]
 
 
 CSRF_TRUSTED_ORIGINS = [
+    "gabriellekourdadze.com",
+    "www.gabriellekourdadze.com/",
+    "http://www.gabriellekourdadze.com/",
     "https://lobster-app-fy7pe.ondigitalocean.app",
     "https://127.0.0.1"
 ]
